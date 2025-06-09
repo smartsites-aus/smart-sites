7b04a6323c35607bd6d3d1bff4957568ae1d2c4b
smartsites-aus <smartsitessystem@gmail.com>
2025-06-09 20:18:21 +1000

    Update Homepage.jsx

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
 import './Homepage.css';
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <h1>Welcome to Smart Sites</h1>
+      <p>Select an option from the sidebar to get started.</p>
+    </div>
+  );
+};
+
+export default Homepage;
