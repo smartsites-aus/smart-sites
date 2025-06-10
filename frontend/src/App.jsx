6ad61059321586ea427fcb0b1cb3918d6cf9b0e4
smartsites-aus <smartsitessystem@gmail.com>
2025-06-10 17:43:46 +1000

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import Homepage from './pages/Homepage.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Homepage from './pages/Homepage';
+import ESPHomePage from './pages/ESPHomePage';
 
 function App() {
-  return <Homepage />;
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-grow p-6 overflow-auto">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/esphome" element={<ESPHomePage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
